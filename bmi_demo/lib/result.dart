561fe569b7844756d26975e551636630cd86fcd2
AyoubFrihaoui <a.frihaoui@esi-sba.dz>
2023-06-11 12:26:03 +0100

    snackBar demoed

--- bmi_demo/lib/result.dart
+++ bmi_demo/lib/result.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Result extends StatelessWidget {
+class Result extends StatefulWidget {
   final double result;
   final bool isMale;
   final int age;
@@ -11,38 +11,44 @@ class Result extends StatelessWidget {
       required this.isMale,
       required this.age});
 
+  @override
+  State<Result> createState() => _ResultState();
+}
+
+class _ResultState extends State<Result> {
   String get resultPhrase {
     String resultText = '';
-    if (result < 18.5)
+    if (widget.result < 18.5)
       resultText = 'Underweight';
-    else if (result >= 18.5 && result < 25)
+    else if (widget.result >= 18.5 && widget.result < 25)
       resultText = 'Normalweight';
-    else if (result >= 25 && result < 30)
+    else if (widget.result >= 25 && widget.result < 30)
       resultText = 'Overweight';
-    else if (result >= 30) resultText = 'Obese';
+    else if (widget.result >= 30) resultText = 'Obese';
 
     return resultText;
   }
 
+  String appBarTitle = 'Result';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Result')),
+      appBar: AppBar(title: Text(appBarTitle)),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               Text(
-                'Gender: ${this.isMale ? 'Male' : 'Female'}',
+                'Gender: ${this.widget.isMale ? 'Male' : 'Female'}',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               Text(
-                'Age: ${this.age}',
+                'Age: ${this.widget.age}',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               Text(
-                'Result: ${this.result.toStringAsFixed(2)}',
+                'Result: ${this.widget.result.toStringAsFixed(2)}',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               Text(
@@ -79,7 +85,39 @@ class Result extends StatelessWidget {
                     height: 40,
                     width: double.infinity,
                     child: Text('Dialog')),
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      appBarTitle = 'hi';
+                    });
+                    final snk = SnackBar(
+                      action: SnackBarAction(
+                          textColor: Color.fromARGB(255, 218, 218, 218),
+                          label: 'Undo',
+                          onPressed: () {
+                            setState(() {
+                              appBarTitle = 'Result';
+                            });
+                          }),
+                      content: Text(
+                        'data',
+                      ),
+                      duration: Duration(milliseconds: 1500),
+                      backgroundColor: Color.fromARGB(255, 223, 105, 105),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snk);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('close'),
+                  ))
             ],
           ),
         ),
