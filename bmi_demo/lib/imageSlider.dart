4c4cf8d0d8ad2ef5b9d9eb009dd768cb34cac155
AyoubFrihaoui <a.frihaoui@esi-sba.dz>
2023-07-04 22:22:05 +0100

    checklisttitle demoed

--- bmi_demo/lib/imageSlider.dart
+++ bmi_demo/lib/imageSlider.dart
@@ -16,58 +16,73 @@ class _imageSliderState extends State<imageSlider> {
       'assets/images/s2.jpg',
       'assets/images/s3.jpg'
     ];
+
+    bool? cSharpBool = true;
     return Scaffold(
       appBar: AppBar(title: Text('Image slider')),
       body: Container(
           alignment: Alignment.center,
           color: backgroudColor,
-          child: ListView(children: [
-            SizedBox(
-              height: 30,
-            ),
-            CarouselSlider(
-              options: CarouselOptions(
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 1),
-                  height: 186,
-                  aspectRatio: 16 / 9,
-                  enlargeCenterPage: true),
-              items: images
-                  .map((imgurl) => Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10),
-                        width: double.infinity,
-                        child: Image.asset(
-                          imgurl,
-                          fit: BoxFit.fill,
-                        ),
-                      ))
-                  .toList(),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text('Guess the answer of 1+1:'),
-            RadioListTile(
-              value: Color.fromARGB(255, 255, 255, 255),
-              groupValue: backgroudColor,
-              onChanged: (value) {
-                setState(() {
-                  backgroudColor = value!;
-                });
-              },
-              title: Text('White'),
-            ),
-            RadioListTile(
-              value: Color.fromARGB(255, 41, 41, 41),
-              groupValue: backgroudColor,
-              onChanged: (value) {
-                setState(() {
-                  backgroudColor = value!;
-                });
-              },
-              title: Text('Black'),
-            )
-          ])),
+          child: StatefulBuilder(builder:
+              (BuildContext context, void Function(void Function()) setState) {
+            return ListView(children: [
+              SizedBox(
+                height: 30,
+              ),
+              CarouselSlider(
+                options: CarouselOptions(
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 1),
+                    height: 186,
+                    aspectRatio: 16 / 9,
+                    enlargeCenterPage: true),
+                items: images
+                    .map((imgurl) => Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          width: double.infinity,
+                          child: Image.asset(
+                            imgurl,
+                            fit: BoxFit.fill,
+                          ),
+                        ))
+                    .toList(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text('Guess the answer of 1+1:'),
+              RadioListTile(
+                value: Color.fromARGB(255, 255, 255, 255),
+                groupValue: backgroudColor,
+                onChanged: (value) {
+                  setState(() {
+                    backgroudColor = value!;
+                  });
+                },
+                title: Text('White'),
+              ),
+              RadioListTile(
+                value: Color.fromARGB(255, 41, 41, 41),
+                groupValue: backgroudColor,
+                onChanged: (value) {
+                  setState(() {
+                    backgroudColor = value!;
+                  });
+                },
+                title: Text('Black'),
+              ),
+              CheckboxListTile(
+                  controlAffinity: ListTileControlAffinity.leading,
+                  title: Text('C#'),
+                  value: cSharpBool,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      cSharpBool = value!;
+                      print(cSharpBool);
+                    });
+                  })
+            ]);
+          })),
     );
     throw UnimplementedError();
   }
